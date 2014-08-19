b655e425fc991e89f995ed4bd9700f229f0cd091
Zeljko <zeljko@zwr.fi>
2014-08-19 18:23:36 +0300

    intuitive routes login and logout
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :categories
+  devise_scope :user do
+    get 'login' => 'devise/sessions#new'
+    get 'logout' => 'devise/sessions#destroy'
+  end
 
   devise_for :users
   get 'home/get'
