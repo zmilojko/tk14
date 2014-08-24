e39c1bcb259c2970eb985ee20923bd37606380f4
Zeljko <zeljko@zwr.fi>
2014-08-24 11:27:38 +0300

    add fields to user table
    Signed-off-by: Zeljko <zeljko@zwr.fi>

---
+++ db/migrate/20140824081930_add_fields_to_user.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :display_name, :string
+    add_column :users, :full_name, :string
+    add_column :users, :profile, :string
+    add_column :users, :pictures, :string
+    add_column :users, :is_racer, :boolean
+  end
+end
