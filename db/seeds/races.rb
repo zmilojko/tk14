9eada17c41af1c155c2e00c2c5fbf49867701127
Zeljko <zeljko@zwr.fi>
2014-09-01 13:11:37 +0300

    Race seeds: first day races all closed
    Signed-off-by: Zeljko <zeljko@zwr.fi>

--- db/seeds/races.rb
+++ db/seeds/races.rb
@@ -2,7 +2,8 @@ p 'Creating races'
 
 @spu_day1 = Race.create! category: @spu,
                          race_time: Time.new(2014, 3, 7, 10),
-                         name: 'SpU Day 1'
+                         name: 'SpU Day 1',
+                         status: :closed
 @spu_day2 = Race.create! category: @spu,
                          race_time: Time.new(2014, 3, 8, 10),
                          name: 'SpU Day 2',
@@ -11,7 +12,9 @@ p 'Creating races'
 
 @sp8_day1 = Race.create! category: @sp8,
                          race_time: Time.new(2014, 3, 7, 11),
-                         name: 'Sp8 Day 1'
+                         name: 'Sp8 Day 1',
+                         status: :closed
+
 @sp8_day2 = Race.create! category: @sp8,
                          race_time: Time.new(2014, 3, 8, 11),
                          name: 'Sp8 Day 2',
@@ -20,7 +23,9 @@ p 'Creating races'
 
 @sp6_day1 = Race.create! category: @sp6,
                          race_time: Time.new(2014, 3, 7, 12),
-                         name: 'Sp6 Day 1'
+                         name: 'Sp6 Day 1',
+                         status: :closed
+
 @sp6_day2 = Race.create! category: @sp6,
                          race_time: Time.new(2014, 3, 7, 12),
                          name: 'Sp6 Day 2',
@@ -30,7 +35,9 @@ p 'Creating races'
 
 @sp4_day1 = Race.create! category: @sp4,
                          race_time: Time.new(2014, 3, 7, 13),
-                         name: 'Sp4 Day 1'
+                         name: 'Sp4 Day 1',
+                         status: :closed
+
 @sp4_day2 = Race.create! category: @sp4,
                          race_time: Time.new(2014, 3, 7, 13),
                          name: 'Sp4 Day 2',
@@ -39,7 +46,9 @@ p 'Creating races'
 
 @sp4j_day1 = Race.create! category: @sp4j,
                          race_time: Time.new(2014, 3, 7, 13, 30),
-                         name: 'Sp4J Day 1'
+                         name: 'Sp4J Day 1',
+                         status: :closed
+
 @sp4j_day2 = Race.create! category: @sp4j,
                          race_time: Time.new(2014, 3, 7, 13, 30),
                          name: 'Sp4J Day 2',
@@ -48,7 +57,9 @@ p 'Creating races'
 
 @md12_day1 = Race.create! category: @md12,
                          race_time: Time.new(2014, 3, 7, 14),
-                         name: 'MD12 Day 1'
+                         name: 'MD12 Day 1',
+                         status: :closed
+
 @md12_day2 = Race.create! category: @md12,
                          race_time: Time.new(2014, 3, 7, 14),
                          name: 'MD12 Day 2',
@@ -58,9 +69,11 @@ p 'Creating races'
 
 @md6_day1 = Race.create! category: @md6,
                          race_time: Time.new(2014, 3, 7, 15, 30),
-                         name: 'MD6 Day 1'
+                         name: 'MD6 Day 1',
+                         status: :closed
+
 @md6_day2 = Race.create! category: @md6,
                          race_time: Time.new(2014, 3, 7, 15, 30),
                          name: 'MD6 Day 2',
                          status: :completed,
-                         antermediate_points: "5km 10km 15km"
+                         intermediate_points: "5km 10km 15km"
